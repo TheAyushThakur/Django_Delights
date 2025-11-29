00d08a2ff0018e97dd95d8f226aad77bbabc9e82
TheAyushThakur <ayushthakur6005@gmail.com>
2025-11-29 11:41:05 +0530

    Added a default value to quanity in purchase model

--- inventory/models.py
+++ inventory/models.py
@@ -27,7 +27,7 @@ class RecipieRequirements(models.Model):
 
 class Purchase(models.Model):
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField(default=1)
     time_stamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
