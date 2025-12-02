f068c4510a35086c2277821040e49317250a4280
TheAyushThakur <ayushthakur6005@gmail.com>
2025-12-02 14:58:13 +0530

    Created forms with respect to all the models

---
+++ inventory/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import Ingredients, MenuItem, RecipieRequirements, Purchase
+
+class IngredientForm(forms.ModelForm):
+    class Meta:
+        model = Ingredients
+        fields = ['name', 'quantity', 'unit', 'price_per_unit']
+
+class MenuItemForm(forms.ModelForm):
+    class Meta:
+        model = MenuItem
+        fields = ['title', 'price']
+
+class RecipieRequirementsForm(forms.ModelForm):
+    class Meta:
+        model = RecipieRequirements
+        fields = ['menu_item', 'ingredient', 'quantity']
+
+class PurchaseForm(forms.ModelForm):
+    class Meta:
+        model = Purchase
+        fields = ['menu_item', 'quantity']
